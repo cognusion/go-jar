7ef14fc30b230756f6970fa714e31ac2a6bc4892
M <m@cognusion.com>
2022-08-22 14:35:55 -0400

    Adds Resolve test for SyncDict

--- dictionary/syncdict_test.go
+++ dictionary/syncdict_test.go
@@ -26,6 +26,36 @@ func TestSyncDict(t *testing.T) {
 
 }
 
+func TestSyncDictResolve(t *testing.T) {
+
+	var s SyncDict
+	s.AddValues(map[string]string{
+		"NAME":         "Tester",
+		"VERSION":      "1.0",
+		"SHORTVERSION": "%%NAME/%%VERSION",
+		"FULLVERSION":  "%%SHORTVERSION Moar Words %%NAME",
+	})
+	s.Resolve()
+
+	Convey("When a SimpleDict is created and then Resolve(), strings containing dictionary items are properly replaced", t, func() {
+		So(s.Replacer("My name is %%NAME"), ShouldEqual, "My name is Tester")
+
+		Convey("and dictionary items containing other dictionary items are properly expanded", func() {
+			exp := s.Replacer("%%NAME %%VERSION %%SHORTVERSION")
+			So(exp, ShouldEqual, "Tester 1.0 Tester/1.0")
+		})
+
+		Convey("and strings containing two levels of dictionary items are properly replaced", func() {
+			So(s.Replacer("%%FULLVERSION"), ShouldEqual, "Tester/1.0 Moar Words Tester")
+		})
+
+		Convey("and strings with dictionary-item-looking substrings do not hang up the processing", func() {
+			So(s.Replacer("This %%FULLVERSION is %%NOTHING"), ShouldEqual, "This Tester/1.0 Moar Words Tester is %%NOTHING")
+		})
+	})
+
+}
+
 func TestSyncDict2ndLevelRecursion(t *testing.T) {
 
 	var s SyncDict
