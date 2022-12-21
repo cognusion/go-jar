117e922fd2a47c80eaaa1e943f5709cbe298da2b
M <m@cognusion.com>
2022-12-21 18:12:53 -0500

    Expands tus Finisher setup to enforce target prefixes.

--- tus.go
+++ tus.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cognusion/go-jar/tus"
 
 	"net/http"
+	"strings"
 )
 
 /*
@@ -23,13 +24,27 @@ func init() {
 	// Set up the static finishers
 	Finishers["tus"] = nil
 	FinisherSetups["tus"] = func(p *Path) (http.HandlerFunc, error) {
-		var targetURI string
+		var (
+			targetURI string
+			t         *tus.TUS
+			err       error
+		)
 		if targetURI = p.Options.GetString(ConfigTUSTargetURI); targetURI == "" {
+			// Missing target!
 			return nil, ErrTUSTargetURIMissing
+		} else if strings.HasPrefix(strings.ToLower(targetURI), "s3://") {
+			// S3 target
+			t, err = tus.NewTUSwithS3(targetURI, p.Path, AWSSession.S3Client())
+		} else if strings.HasPrefix(strings.ToLower(targetURI), "file://") {
+			// File target
+			t, err = tus.NewTUS(targetURI, p.Path)
+		} else {
+			// Bad prefix
+			err = tus.ErrBadTargetPrefix
 		}
-		t, terr := tus.NewTUS(targetURI, p.Path)
-		if terr != nil {
-			return nil, terr
+
+		if err != nil {
+			return nil, err
 		}
 		return http.StripPrefix(p.Path, t).ServeHTTP, nil
 	}
