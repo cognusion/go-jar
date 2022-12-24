e6c904943e137ceaae1e59795ba34ef850e923ed
M <m@cognusion.com>
2022-12-24 12:47:27 -0500

    Implements post-completion renaming.
    ... as the TUS name doesn't even have the extension, this can make
    downloading from other than a TUS client problematic. We can now
    rename the resulting file to `hash-filename.ext` after the upload
    is complete.

--- tus.go
+++ tus.go
@@ -16,9 +16,9 @@ import (
 
 // Constants for configuration key strings and Errors
 const (
-	ConfigTUSTargetURI       = ConfigKey("tus.targeturi")
-	ConfigTUSAppendExtension = ConfigKey("tus.appendextension")
-	ErrTUSTargetURIMissing   = Error("tus.targeturi missing from path options")
+	ConfigTUSTargetURI      = ConfigKey("tus.targeturi")
+	ConfigTUSAppendFilename = ConfigKey("tus.appendfilename")
+	ErrTUSTargetURIMissing  = Error("tus.targeturi missing from path options")
 )
 
 func init() {
@@ -28,17 +28,27 @@ func init() {
 		var (
 			targetURI string
 			t         *tus.TUS
+			conf      tus.Config
 			err       error
 		)
 		if targetURI = p.Options.GetString(ConfigTUSTargetURI); targetURI == "" {
 			// Missing target!
 			return nil, ErrTUSTargetURIMissing
-		} else if strings.HasPrefix(strings.ToLower(targetURI), "s3://") {
+		}
+
+		// Prep config
+		conf = tus.Config{
+			TargetURI:      p.Options.GetString(ConfigTUSTargetURI),
+			AppendFilename: p.Options.GetBool(ConfigTUSAppendFilename),
+		}
+
+		if strings.HasPrefix(strings.ToLower(targetURI), "s3://") {
 			// S3 target
-			t, err = tus.New(p.Path, tus.Config{TargetURI: targetURI, S3Client: AWSSession.S3Client()})
+			conf.S3Client = AWSSession.S3Client()
+			t, err = tus.New(p.Path, conf)
 		} else if strings.HasPrefix(strings.ToLower(targetURI), "file://") {
 			// File target
-			t, err = tus.New(p.Path, tus.Config{TargetURI: targetURI})
+			t, err = tus.New(p.Path, conf)
 		} else {
 			// Bad prefix
 			err = tus.ErrBadTargetPrefix
@@ -52,5 +62,6 @@ func init() {
 
 	InitFuncs.Add(func() {
 		tus.DebugOut = DebugOut
+		tus.ErrorOut = ErrorOut
 	})
 }
